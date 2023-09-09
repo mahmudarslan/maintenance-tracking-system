b0cfd8171e7b84826e409b1d226c81c3391fbb7f
mahmudarslan <mahmudarslan@gmail.com>
2023-09-09 20:47:32 +0300

    docker compose

--- etc/docker/up.ps1
+++ etc/docker/up.ps1
@@ -1,2 +1,2 @@
-docker network create Arslan.Vms-network
+docker network create arslan.vms-network
 docker-compose -f docker-compose.infrastructure.yml -f docker-compose.infrastructure.override.yml up -d
